f3e2cc782156c442bec9a35f46c75e7a0552b6f7
Scalene Scales <scalene-scales@users.noreply.github.com>
2022-11-02 23:07:42 -0700

    Add Scalene Scales specific icons.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import 'bootstrap/dist/css/bootstrap.css';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button type="button" className='btn btn-primary'>Primary</button>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
